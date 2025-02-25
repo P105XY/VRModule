de21d0e217287cdc31c1fc47b2926965fad7db97
P105XY <p105xy85@gmail.com>
2025-02-25 11:41:43 +0900

    소스코드 정리 및 추가.

---
+++ Source/VirtualTracker/VirtualTracker.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VirtualTracker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VirtualTracker, "VirtualTracker" );
